332f9a761917f5a58bfed17431c61a1580c7bb86
Sota Makino <sotamaki0421@gmail.com>
2022-02-11 13:08:36 +0900

    レンダリング最適化2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { useState, memo } from "react";
+import { useState, memo, useCallback } from "react";
 import { Child1 } from "./components/Child1";
 import { Child4 } from "./components/Child4";
+import "./index";
 
 export const App = memo(() => {
   console.log("App レンダリング");
@@ -11,11 +12,21 @@ export const App = memo(() => {
     setNum(num + 1);
   };
 
+  const onClickReset = useCallback(() => {
+    setNum(0);
+  }, []);
+
   return (
     <>
-      <button onClick={onClickButton}>ボタン</button>
+      <button
+        onClick={onClickButton}
+        className="bg-gray-300 border-0 p-2 rounded-md hover:bg-gray-400 hover:text-white"
+      >
+        ボタン
+      </button>
       <p>{num}</p>
-      <Child1 />
+      {/* ↓Propsとして関数を設定 */}
+      <Child1 onClickReset={onClickReset} />
       <Child4 />
     </>
   );
