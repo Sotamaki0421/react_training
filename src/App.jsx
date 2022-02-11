43c094b573d9c1a39f2d132ad997c44740dac0f5
Sota Makino <sotamaki0421@gmail.com>
2022-02-11 12:47:39 +0900

    レンダリング最適化1

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,22 @@
-import { useState } from "react";
-// import { ColoredMessage } from "./components/ColoredMessage";
-// import { CssModules } from "./components/CssModules";
-import { TailwindCss } from "./components/TailwindCss";
+import { useState, memo } from "react";
+import { Child1 } from "./components/Child1";
+import { Child4 } from "./components/Child4";
 
-export const App = () => {
-  console.log("レンダリング");
+export const App = memo(() => {
+  console.log("App レンダリング");
 
-  // Stateの定義
-  // const [num, setNum] = useState(0);
-  // ボタンを押した時に実行する関数を定義
-  // const onClickButton = () => {
-  //   alert();
-  // setNum((prev) => prev + 1);
-  // };
+  const [num, setNum] = useState(0);
+
+  const onClickButton = () => {
+    setNum(num + 1);
+  };
 
   return (
     <>
-      <h1 style={{ color: "red" }}>こんにちは！</h1>
-      {/* <ColoredMessage color="blue">お元気ですか？</ColoredMessage> */}
-      {/* <ColoredMessage color="pink">元気です！</ColoredMessage> */}
-      {/* <button onClick={onClickButton}>ボタン</button> */}
-      {/* <CssModules /> */}
-      {/* <p>{num}</p> */}
-      <TailwindCss />
+      <button onClick={onClickButton}>ボタン</button>
+      <p>{num}</p>
+      <Child1 />
+      <Child4 />
     </>
   );
-};
+});
